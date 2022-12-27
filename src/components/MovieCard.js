77c0def02ebff99d0431b2c119088c9c7b14ac99
ayedibtissem <116677898+ayedibtissem@users.noreply.github.com>
2022-12-27 16:33:12 +0100

    Add files via upload

---
+++ src/components/MovieCard.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Rating from '@mui/material/Rating';
+
+function MovieCard({data}){
+    return ( <div> <Card style={{ width: '18rem' }}>
+    <Card.Img variant="top" src={data.posterUrl} />
+    <Card.Body>
+      <Card.Title>{data.title}</Card.Title>
+      <Card.Text>
+       {data.description}
+      </Card.Text>
+      <Button variant="primary">Go</Button>
+      <Rating name="read-only" value={data.rating} max={10} readOnly />
+
+    </Card.Body>
+  </Card>
+
+</div>
+    );
+}
+export default MovieCard;
---
+++ src/components/data.js
@@ -0,0 +1,18 @@
+const data=[{
+title:"The Batman",
+description:"Batman ventures into Gotham City's underworld when a sadistic killer leaves behind a trail of cryptic clues. ",
+posterUrl:"https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/71kUTilIdiL._AC_UL320_.jpg",
+trailerUrl:"https://www.youtube.com/watch?v=vc7_mH2PWHs",
+rating:5,
+},
+{
+title:"The Superman",
+description:"An alien orphan is sent from his dying planet to Earth, where he grows up to become his adoptive home's first and greatest superhero.",
+posterUrl:"https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/71CM28aP5rL.__AC_SX300_SY300_QL70_FMwebp_.jpg",
+trailerUrl:"https://www.youtube.com/watch?v=FLB3mK_t9kE",
+rating:8,
+
+},
+
+]
+export default data;
